1545af7697488b115c25ccc5ad9c7364b38e61d1
Jade Lilian <lilianpalosky@gmail.com>
2025-02-11 13:47:16 -0500

    technical requirements complete

--- CSC209/homeworks/homework2/assets/javascript/buttons.js
+++ CSC209/homeworks/homework2/assets/javascript/buttons.js
@@ -1,5 +1,16 @@
 function darkToggle() {
   let on = document.body.classList.toggle("dark-mode");
-  on ? document.getElementById("dark-button").innerHTML = "light mode" :
-    document.getElementById("dark-button").innerHTML = "dark mode";
+  on ? document.getElementById("dark-button").innerHTML = "Light Mode" :
+    document.getElementById("dark-button").innerHTML = "Dark Mode";
+}
+function describeMe() {
+  let description = document.getElementById("description");
+  let hidden = description.classList.toggle("hidden");
+  if (hidden) {
+    description.innerHTML = "";
+    document.getElementById("read-me").innerHTML = "Show more";
+  } else {
+    description.innerHTML = "[[describe here]]";
+    document.getElementById("read-me").innerHTML = "Show less";
+  }
 }
