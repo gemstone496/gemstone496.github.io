2a290651f3784bbf4d01cce0875775f476bd5fe6
Jade Lilian <lilianpalosky@gmail.com>
2025-02-24 23:43:13 -0500

    living on a prayer

--- CSC209/homeworks/homework4/assets/javascript/animation.js
+++ CSC209/homeworks/homework4/assets/javascript/animation.js
@@ -1,13 +1,54 @@
+/**
+ * Sets up the divs for each anima tile
+ */
 function initAnimation() {
-  document.getElementById("initializer").textContent = "Reset";
-  let tiles
+  console.log("Setting up...")
+  let box, dir;
+
+  document.getElementById("initializer").textContent = "Reset"; // set up once, then reset after
+
+  box = document.getElementById("animation-container")
+  const count = box.dataset.count;
+  dir = -3*Math.PI / 4;
+  const inc = 2*Math.PI / count; // if count == 0 this will throw an error and it should
+
+  for (let i=0; i < count; i++) {
+    console.log(`Inserting anima[ble] #${count}.`)
+
+    // set up container
+    let anima = document.createElement("div");
+    anima.classList = "anima";
+
+    /* so i just need the inscribed circle based on the size of the box 
+        and also the size of the animables and i need to find it
+        based on a single edit in code for varying widths of both
+        and i want it to work for rectangular non-square objects of all sizes 
+        what could go wrong */
+    const width = anima.style.width/2, height = anima.style.height/2;
+    let rad = box.style.width - width < box.style.height - height ? 
+              box.style.width - width : box.style.height - height;
+    
+    let x = rad * Math.cos(dir) - width;
+    let y = rad * Math.sin(dir) - height;
+    anima.style.left = x;
+    anima.style.top = y;
+    box.appendChild(anima);
+    dir += inc; 
+  }
 }
 
+/**
+ * Begins the entire animation sequence for all tiles
+ */
 function begin() {
+  console.log("Beginning animation...")
+  let tiles, dir;
+
   document.getElementById("animate-start").disabled = true;
-  let tiles = document.getElementsByClassName("anima");
-  let speed = document.getElementById("tile-speed").value;
-  let dir = Math.PI/4, inc = 2*Math.PI / tiles.length;
+  tiles = document.getElementsByClassName("anima");
+  dir = Math.PI/4;
+  const inc = 2*Math.PI / tiles.length;
+  const speed = document.getElementById("tile-speed").value;
 
   for (let tile of tiles) {
     let dx = speed * Math.cos(dir);
@@ -17,14 +58,20 @@ function begin() {
   }
 }
 
+/**
+ * Triggers the animation sequence for tile, with fixed direction dx/dt and dy/dt
+ * @param {*} tile The tile being animated
+ * @param {*} dx dx/dt
+ * @param {*} dy dy/dt
+ */
 function moveTile(tile, dx, dy) {
-  var x = tile.style.left;
+  var x = tile.style.left; // where am i now
   var y = tile.style.top;
-  var stepId = setInterval(step, 40); // 25fps
+  var stepId = setInterval(step, 40); // 40ms <==> 25fps
 
   function step() {
-    if (pos == 350) {
-      clearInterval(stepId);
+    if (x == 350 || y == 350) { // edge of box
+      clearInterval(stepId); // finish
     } else {
       x += dx; y += dy; // incr
       tile.style.top = y + 'px';
@@ -33,4 +80,5 @@ function moveTile(tile, dx, dy) {
   }
 }
 
-
+function fuck_math(anima) {
+}
