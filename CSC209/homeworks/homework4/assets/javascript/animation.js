eef76934e2e78b7d0fb0647a487f9fe55fc185e0
Jade Lilian <lilianpalosky@gmail.com>
2025-02-24 20:59:08 -0500

    wo-ah, we're halfway the-ere

--- CSC209/homeworks/homework4/assets/javascript/animation.js
+++ CSC209/homeworks/homework4/assets/javascript/animation.js
@@ -1,15 +1,34 @@
-function moveRed() {   
-  var redSquare = document.getElementById("redSq");   
-  var redPos = 0;
-  var stepRedId = setInterval(stepRed, 10);
+function initAnimation() {
+  document.getElementById("initializer").textContent = "Reset";
+  let tiles
+}
 
-  function stepRed() {
-    if (redPos == 350) {
-      clearInterval(stepRedId);
+function begin() {
+  document.getElementById("animate-start").disabled = true;
+  let tiles = document.getElementsByClassName("anima");
+  let speed = document.getElementById("tile-speed").value;
+  let dir = Math.PI/4, inc = 2*Math.PI / tiles.length;
+
+  for (let tile of tiles) {
+    let dx = speed * Math.cos(dir);
+    let dy = speed * Math.sin(dir);
+    moveTile(tile, dx, dy);
+    dir += inc;
+  }
+}
+
+function moveTile(tile, dx, dy) {
+  var x = tile.style.left;
+  var y = tile.style.top;
+  var stepId = setInterval(step, 40); // 25fps
+
+  function step() {
+    if (pos == 350) {
+      clearInterval(stepId);
     } else {
-      redPos++; 
-      redSquare.style.top = redPos + 'px'; 
-      redSquare.style.left = redPos + 'px';
+      x += dx; y += dy; // incr
+      tile.style.top = y + 'px';
+      tile.style.left = x + 'px';
     }
   }
 }
