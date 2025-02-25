13b235403ec9edb5346f71e8ded1d4afe0007821
Jade Lilian <lilianpalosky@gmail.com>
2025-02-25 18:45:42 -0500

    still bugged

--- CSC209/homeworks/homework4/assets/javascript/animation.js
+++ CSC209/homeworks/homework4/assets/javascript/animation.js
@@ -1,5 +1,58 @@
-// for tracking ongoing intervals
-var intervals = new Map();
+/** for tracking existing tiles */
+var tiles = new Array();
+
+/** an animable tile */
+class Tile {
+  /**
+   * Default constructor
+   * @param {HTMLDivElement} element 
+   * @param {Number} theta 
+   * @param {Array} bounds 
+   */
+  constructor(element, theta, bounds) {
+    this.element = element;
+    this.r = 0;
+    this.theta = theta;
+    this.bounds = bounds;
+    this.stepId = null;
+    this.active = false;
+  }
+
+  ddt() { return [this.r*Math.cos(this.theta), this.r*Math.sin(this.theta)]; }
+
+  move() {
+    var x = dimension(this.element.style.left); // where am i now
+    var y = dimension(this.element.style.top);
+    var ddt = this.ddt(); // evaluate once for now, update later when appropriate
+    this.stepId = setInterval(step, 40); // 40ms <==> 25fps
+    this.active = true;
+
+    function step() {
+      // check distance from center
+      if (edgeFound(this.dx, this.dy)) { // edge of box
+        console.log(`ending #${this.stepId} due to tile edge`);
+        finishInterval(this.stepId);
+      } else {
+        x += ddt[0]; y += ddt[1]; // incr
+        x = Math.max(bounds[0], Math.min(x, bounds[1]));
+        y = Math.max(bounds[2], Math.min(y, bounds[3]));
+        this.element.style.left = x + 'px';
+        this.element.style.top = y + 'px';
+      }
+      
+      /**
+       * Decides whether an object is about to cross a bound
+       * @returns {Boolean} whether tile should stop moving
+       */
+      function edgeFound() {
+        return (ddt[0] < 0 && x <= bounds[0]) ||
+               (ddt[0] > 0 && x >= bounds[1]) ||
+               (ddt[1] < 0 && y <= bounds[2]) ||
+               (ddt[1] > 0 && y >= bounds[3]);
+      }
+    }
+  }
+}
 
 /**
  * Sets up the divs for each anima tile
@@ -10,6 +63,7 @@ function initAnimation(button) {
   console.log('Setting up...')
 
   button.textContent == 'Reset' ? purgeTiles() : button.textContent = 'Reset'; // set up once, then reset after
+  stopAnimation();
 
   box = document.getElementById('animation-container');
   boxStyle = window.getComputedStyle(box);
@@ -27,19 +81,22 @@ function initAnimation(button) {
     animable = box.appendChild(animable);
     animaStyle = window.getComputedStyle(animable);
 
-    /* so i just need the inscribed circle based on the size of the box 
-        and also the size of the animables and i need to find it
-        based on a single edit in code for varying widths of both
-        and i want it to work for rectangular non-square objects of all sizes 
-        what could go wrong */
+    /* inscribed circle based on the size of box and animables 
+       from single edit in code for varying widths of both.
+       works for rectangular non-square objects of all sizes */
     const width = dimension(animaStyle.width), height = dimension(animaStyle.height);
-    rad = Math.min( (dimension(boxStyle.width) - width)/2, 
-                    (dimension(boxStyle.height) - height)/2);
+    const eastBound = dimension(boxStyle.width) - width;
+    const southBound = dimension(boxStyle.height) - height;
+    rad = Math.min( eastBound / 2, southBound / 2);
     
-    x = rad * Math.cos(dir) + (dimension(boxStyle.width)-width)/2;
-    y = rad * Math.sin(dir) + (dimension(boxStyle.height)-height)/2;
+    x = rad * Math.cos(dir) + eastBound / 2;
+    y = rad * Math.sin(dir) + southBound / 2;
     animable.style.left = x + 'px';
     animable.style.top = y + 'px';
+
+    const bounds = [0, eastBound, 0, southBound];
+    tiles.push(new Tile(animable, dir+5*Math.PI/4, bounds));
+
     dir += inc;
   }
 }
@@ -52,63 +109,11 @@ function begin(button) {
   console.log('Beginning animation...')
 
   button.disabled = true;
-  const tiles = document.getElementsByClassName('animable');
-  let dir = -Math.PI/2;
-  const inc = 2*Math.PI / tiles.length;
   const speed = document.getElementById('tile-speed').value;
 
   for (const tile of tiles) {
-    moveTile(tile, speed, dir);
-    dir += inc;
-  }
-}
-
-/**
- * Triggers the animation sequence for tile, with fixed direction dx/dt and dy/dt
- * @param {HTMLDivElement} tile The tile being animated
- * @param {Number} r polar radius
- * @param {Number} theta polar angle theta
- */
-function moveTile(tile, r, theta) {
-  let box, boxStyle;
-  var tileStyle = window.getComputedStyle(tile);
-  var ddt = cartesian(r, theta);
-
-  // define bounds
-  box = document.getElementById('animation-container');
-  boxStyle = window.getComputedStyle(box);
-  const left = 0, right = dimension(boxStyle.width) - dimension(tileStyle.width), 
-        top = 0, bottom = dimension(boxStyle.height) - dimension(tileStyle.height);
-  
-  var x = dimension(tile.style.left); // where am i now
-  var y = dimension(tile.style.top);
-  var stepId = setInterval(step, 40); // 40ms <==> 25fps
-  intervals[stepId] = true;
-
-  function step() {
-    // check distance from center
-    if (edgeFound()) { // edge of box
-      console.log(`ending #${stepId} due to tile edge`);
-      finishInterval(stepId);
-    } else {
-      x += ddt[0]; y += ddt[1]; // incr
-      x = Math.max(left, Math.min(x, right));
-      y = Math.max(top, Math.min(y, bottom));
-      tile.style.left = x + 'px';
-      tile.style.top = y + 'px';
-    }
-
-    /**
-     * Decides whether an object is about to cross a bound.
-     * @returns {Boolean} whether tile should stop moving
-     */
-    function edgeFound(){
-      let stop = (ddt[0] < 0 && x <= left) ||
-                 (ddt[0] > 0 && x >= right) ||
-                 (ddt[1] < 0 && y <= top) ||
-                 (ddt[1] > 0 && y >= bottom);
-      return stop;
-    }
+    tile.r = speed;
+    tile.move();
   }
 }
 
@@ -126,10 +131,10 @@ function dimension(style) {
  */
 function finishInterval(id) {
   clearInterval(id); // finish
-  intervals[id] = false;
+  tiles[id] = false;
 
   let finished = true;
-  for (const entry of intervals.entries()) {
+  for (const tile of tiles) {
     if (entry[1]) {
       finished = false;
       break;
@@ -143,9 +148,6 @@ function finishInterval(id) {
 
 /** removes all existing tiles and all intervals to prep for recreation */
 function purgeTiles() {
-
-  stopAnimation();
-
   let tiles = document.getElementsByClassName('animable'), len = tiles.length;
   for(let i=0; i<len; i++) {
     console.log(`Removing tile #${i}`)
@@ -155,11 +157,11 @@ function purgeTiles() {
 
 /** ends all animations */
 function stopAnimation() {
-  for (const [id, going] of intervals.entries()) {
-    if (going) {
-      console.log(`purging animation #${id}...`)
-      clearInterval(id);
-      intervals[id] = false;
+  for (const tile of tiles) {
+    if (tile.active) {
+      console.log(`purging animation #${tile.stepId}...`)
+      clearInterval(tile.stepId);
+      tile.active = false;
     }
   }
   document.getElementById('animate-start').disabled = false;
