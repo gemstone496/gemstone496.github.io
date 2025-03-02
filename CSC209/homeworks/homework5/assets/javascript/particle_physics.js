af3a5a03f084a90e584a4c2a70b971d042a8f956
Jade Lilian <lilianpalosky@gmail.com>
2025-03-02 12:00:24 -0500

    collider written and only mildly awful math

--- CSC209/homeworks/homework5/assets/javascript/particle_physics.js
+++ CSC209/homeworks/homework5/assets/javascript/particle_physics.js
@@ -3,27 +3,45 @@ const CVS_HGT = 300;
 const RADIUS = 5;
 const MIN_VEL = 2;
 const RANGE_VEL = 10; // [2, 12]
-const HEX = '0123456789ABCDEF';
-const ARW_SCALAR = 1.5; // 10 pixels * velocity
+const HEX = '0123456789ABCDEF'; // hex code characters
+const ARW_SCALAR = 1.5; // pixels * velocity
 const ARH_LEN = 5; // arrowhead length
 const ARH_ANG = 3*Math.PI / 4;
 const FADE = '#ebe1ff33'
-const FPS = 40; // 25fps
+const FPS = 40; // 40ms = 25fps
 var stage;
 var particles = [];
-var freezer = []; // the thing that freezes initial poses
 
-function begin() { stage.begin(); }
-function freeze() { stage.stop(); }
+/**
+ * toggles the state of the animation and updates the button accordingly
+ * @param {HTMLButtonElement} button the button being pressed
+ */
+function toggleAnimation(button) {
+  let mode = button.dataset.mode;
+  console.log
+  mode == 'start' ? stage.begin() : stage.stop();
+  button.textContent = mode == 'start' ? 'FREEZE' : 'Dance!';
+  button.dataset.mode = mode == 'start' ? 'stop' : 'start';
+}
+function begin() {
+  stage.begin();
+  // document.getElementById('animate-start').disabled = true;
+  // document.getElementById('animate-stop').disabled = false;
+}
+function freeze() {
+  stage.stop();
+  // document.getElementById('animate-start').disabled = false;
+  // document.getElementById('animate-stop').disabled = true;
+}
 
 function updateParts() {
   let input = document.getElementById('particle-count');
-  input.value = Math.floor(input.value);
+  input.value = Math.max(1, Math.floor(input.value)); // no lower than 1, no decimals
   let count = input.value;
   while (count != particles.length) {
     if (count < particles.length) {
       particles.pop();
-      if (count == particles.length && stage.static) { // reset without new particles
+      if (count == particles.length && stage.static) { // reset without purged particles
         stage.clear();
         for (let particle of particles) {
           particle.draw();
@@ -36,11 +54,11 @@ function updateParts() {
 }
 
 /** 
- * Generates a Memory object with an initial information for a Particle, 
- * adds it to the list, then creates and returns the Particle
+ * Generates a Particle, adds it to the list, then returns it
  */
 function particleBuilder() {
-  let r, x, y, v, theta, orient, rgb, color, memory;
+  // randomizer
+  let r, x, y, v, theta, orient, rgb, color, particle, count;
   r = RADIUS;
   x = r + Math.floor(Math.random() * (stage.canvas.width -2*r +1)); // stay inside
   y = r + Math.floor(Math.random() * (stage.canvas.height -2*r +1));
@@ -60,7 +78,23 @@ function particleBuilder() {
                 HEX.charAt(rgb[2]) + HEX.charAt(rgb[3]) + 
                 HEX.charAt(rgb[4]) + HEX.charAt(rgb[5]);
   
-  let particle = new Particle(r, x, y, v, theta, orient, color);
+  // construct
+  particle = new Particle(r, x, y, v, theta, orient, color);
+
+  // no overlap
+  if (document.getElementById('collider').checked) {
+    count = 0;
+    while (particle.crashed(particles, 0) !== null) {
+      if (count >= 50) { // too many particles
+        console.error(`Saturation reached :(`);
+        break;
+      }
+      particle.inc(); // sorta bad way to do this but i don't wanna do extra work
+      count++;
+    }
+  }
+
+  // push and draw
   particles.push(particle);
   particle.draw();
   return particle;
@@ -75,14 +109,10 @@ function sceneSet() {
       this.stop();
       this.interval = setInterval(newFrame, FPS);
       this.static = false;
-      document.getElementById('animate-start').disabled = true;
-      document.getElementById('animate-stop').disabled = false;
     },
     stop : function() {
       clearInterval(this.interval);
       this.static = true;
-      document.getElementById('animate-start').disabled = false;
-      document.getElementById('animate-stop').disabled = true;
     },
     clear : function() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -92,6 +122,7 @@ function sceneSet() {
       this.clear();
       for (let particle of particles) {
         particle.reset();
+        particle.draw();
       }
     },
     regenerate : function() {
@@ -121,8 +152,54 @@ function newFrame() {
   } else if (trace == 0) {
     stage.clear();
   }
+
+  // increment
   for (let particle of particles) {
     particle.inc();
+  }
+  
+  // crash
+  if (document.getElementById('collider').checked == true) {
+    for (let i = 0; i < particles.length; i++){
+      let particle = particles[i];
+      let crasher = particle.crashed(particles, i);
+      if (crasher !== null) {
+        let d, rho_1, rho_2, phi, c, x_1, y_1, x_2, y_2, cot;
+
+        radii = particle.r + crasher.r;
+        d = Math.sqrt((crasher.x - particle.x)^2 + (crasher.y - particle.y)^2);
+        rho_1 = Math.atan2(crasher.y - particle.y, crasher.x - particle.x);
+        rho_2 = Math.PI - rho_1;
+
+        // reflections
+        particle.theta = 2*rho_1 + particle.theta;
+        crasher.theta = 2*rho_2 - crasher.theta;
+
+        // repositions
+        // if (radii > d) {
+        //   // particle reposition
+        //   phi = particle.theta - rho_1;
+        //   c = radii / Math.cos(phi) - d / Math.cos(phi);
+        //   x_1 = c * Math.cos(Math.PI + particle.theta); 
+        //   y_1 = c * Math.sin(Math.PI + particle.theta);
+
+        //   // crasher reposition
+        //   phi = crasher.theta - rho_2;
+        //   c = radii / Math.cos(phi) - d / Math.cos(phi);
+        //   x_2 = c * Math.cos(Math.PI + particle.theta); 
+        //   y_2 = c * Math.sin(Math.PI + particle.theta);
+          
+        //   particle.x += x_1; particle.y += y_1;
+        //   crasher.x += x_2; crasher.y += y_2;
+        // }
+        console.log(`part: (${particle.x}, ${particle.y}); theta: ${particle.theta}`);
+      }
+    }
+  }
+
+  // draw
+  for (let particle of particles) {
+    particle.draw();
   }
 }
 
@@ -166,7 +243,6 @@ class Particle {
       let newPos = this.posEval(newX, newY);
       this.x = newPos[0];
       this.y = newPos[1];
-      this.draw();
     }
 
     this.posEval = function(xNew, yNew) {
@@ -188,25 +264,25 @@ class Particle {
     }
 
     this.reset = function() {
-      this.r = init[0];
-      this.x = init[1];
-      this.y = init[2];
-      this.v = init[3];
-      this.theta = init[4];
-      this.arwOrient = init[5];
-      this.color = init[6];
+      this.r = this.init[0];
+      this.x = this.init[1];
+      this.y = this.init[2];
+      this.v = this.init[3];
+      this.theta = this.init[4];
+      this.arwOrient = this.init[5];
+      this.color = this.init[6];
     }
-  }
-}
 
-class Memory {
-  constructor(r, x, y, v, theta, orient, color) {
-    this.r = r;
-    this.x = x;
-    this.y = y;
-    this.v = v;
-    this.theta = theta;
-    this.arwOrient = orient;
-    this.color = color;
+    this.crashed = function(particleList, start) {
+      for (let i = start; i < particleList.length; i++){
+        let otherParticle = particleList[i];
+        if (this !== otherParticle && 
+            Math.sqrt((otherParticle.x - this.x)**2 + (otherParticle.y - this.y)**2
+            ) <= otherParticle.r + this.r) {
+          return otherParticle;
+        }
+      }
+      return null;
+    }
   }
 }
